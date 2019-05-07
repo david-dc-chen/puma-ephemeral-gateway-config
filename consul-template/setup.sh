ca27826816823608b05006512e23340ddd759672
Chen <cheyu04@ca.com>
2019-05-07 14:01:18 -0700

    update

--- consul-template/setup.sh
+++ consul-template/setup.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
 /opt/consul-template/consul-template -config /opt/consul-template/config.hcl -once
 
 cat /opt/consul-template/company.com.cert > /opt/consul-template/trusted.cert
+echo "" /opt/consul-template/trusted.cert
 cat /opt/consul-template/company.com.cert.ca >> /opt/consul-template/trusted.cert
 
 export SSG_SSL_KEY_PEM=$(cat /opt/consul-template/company.com.key)
 export SSG_SSL_KEY_PEM_CERTS=$(cat /opt/consul-template/trusted.cert)
 
-rm -f /opt/consul-template/company.com.key
-rm -f /opt/consul-template/company.com.cert
-rm -f /opt/consul-template/company.com.cert.ca
-rm -f /opt/consul-template/trusted.cert
-rm -f /opt/consul-template/pass.txt
-rm -f /opt/consul-template/license.xml
+# rm -f /opt/consul-template/company.com.key
+# rm -f /opt/consul-template/company.com.cert
+# rm -f /opt/consul-template/company.com.cert.ca
+# rm -f /opt/consul-template/trusted.cert
+# rm -f /opt/consul-template/pass.txt
+# rm -f /opt/consul-template/license.xml
