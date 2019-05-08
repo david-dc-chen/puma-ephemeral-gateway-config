2e2ba4e19daa5f47521ce3bd462c1bf0645a3fc9
Chen <cheyu04@ca.com>
2019-05-08 09:21:54 -0700

    updated script

--- consul-template/setup.sh
+++ consul-template/setup.sh
@@ -5,10 +5,10 @@ cd /opt/consul-template
 ./consul-template -config config.hcl -once
 
 cat company.com.cert > trusted.cert
-echo "" trusted.cert
+echo "" >> trusted.cert
 cat company.com.cert.ca >> trusted.cert
 
-openssl pkcs12 -export -out ssl.p12 -in trusted.cert -inkey company.com.key -password pass:password
+openssl pkcs12 -export -out /opt/vault/ssl.p12 -in trusted.cert -inkey company.com.key -password pass:password
 
 #export SSG_SSL_KEY_PEM=$(cat company.com.key)
 #export SSG_SSL_KEY_PEM_CERTS=$(cat trusted.cert)
